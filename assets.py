83f29887bb35e48123febc3088ae4896d9e28b9c
3bears-data <77301437+3bears-data@users.noreply.github.com>
2023-06-13 18:19:01 -0300

    Update assets.py

--- assets.py
+++ assets.py
@@ -16,12 +16,51 @@ def mediano(topo):
 
 @asset(group_name="pipelineGroup")
 def baixo(mediano):
-    """Execucao baixa 3"""
+    """Execucao baixa 4"""
     print("Executa baixa"
           )
 
 @asset(group_name="gitGroup")
 def getFromGit():
     """Atualiza os arquivos do grupo pipeline"""
+    import git
+    import os
+    import shutil
 
-    print("Update files")
+    # URL do repositório do GitHub
+    repo_url = 'https://github.com/3bears-data/teste-cidcd-dagster.git'
+
+    # Caminho da pasta onde você deseja salvar o script
+    dest_folder = '/opt/dagster/dagster_home/clone_git'
+
+    # Nome do arquivo a ser baixado
+    file_name = 'assets.py'
+
+    try:
+        shutil.rmtree(dest_folder, ignore_errors=True)
+    except:
+        None
+
+    # Clonando o repositório
+    repo = git.Repo.clone_from(repo_url, dest_folder)
+
+    # Movendo o arquivo baixado para o diretório desejado
+    src_file = os.path.join(dest_folder, file_name)
+    dest_file = os.path.join(dest_folder, file_name)
+    os.rename(src_file, dest_file)
+
+    print('Download concluído!')
+
+    # Caminho da pasta de destino
+    dest_path = '/opt/dagster/dagster_home/dagster_home'
+
+    # Caminho completo do arquivo baixado
+    src_file = os.path.join(dest_folder, file_name)
+
+    # Caminho completo do arquivo de destino
+    dest_file = os.path.join(dest_path, file_name)
+
+    # Copiar o arquivo para a pasta de destino
+    shutil.copy(src_file, dest_file)
+
+    print('Arquivo copiado para a pasta de destino com sucesso!')    
